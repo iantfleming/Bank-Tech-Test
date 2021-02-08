c45ed73246a2ae205cbc7f29ea9ebe725da1d892
Ian Fleming <>
2021-02-08 12:26:09 +0000

    deposit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,9 +11,20 @@ describe Account do
     expect(subject).to respond_to(:balance)
   end
 
+  it 'responds to the method deposit' do
+    expect(subject).to respond_to(:deposit).with(1).argument
+  end
+
   describe '#balance' do
     it 'gives a default balance of 0' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#deposit' do
+    it 'adds a deposit amount to the current balance' do
+      subject.deposit(20)
+      expect(subject.balance).to eq 20
     end
   end
 end
