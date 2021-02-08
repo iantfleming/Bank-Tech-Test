1a68337e47707012fe3170479374292da5ec5be6
Ian Fleming <>
2021-02-08 17:06:22 +0000

    interaction between account and statement

--- lib/account.rb
+++ lib/account.rb
@@ -10,15 +10,18 @@ class Account
   def initialize(balance = 0)
     @balance = balance
     @balance_min = BALANCE_MIN
+    @bank_statement = Statement.new
   end
 
   def deposit(amount)
+    @bank_statement.record_transaction(Time.new.strftime('%Y-%m-%d'), 0, amount, balance + amount)
     @balance += amount
   end
 
   def withdraw(amount)
     raise "Balance cannot fall below #{Account::BALANCE_MIN}" if withdraw_over_limit?(amount)
 
+    @bank_statement.record_transaction(Time.new.strftime('%Y-%m-%d'), amount, 0, balance - amount)
     @balance -= amount
   end
 
